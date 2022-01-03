07452f7e8798bbf0e3a46f40026c68ff34955e1d
Ola <Ola180483@feng.bu.edu.eg>
2022-01-03 02:07:21 +0200

    wcat

--- Wcat/Wcat.c
+++ Wcat/Wcat.c
@@ -3,33 +3,33 @@
 #include <stdlib.h>    // exit
 #include <string.h>    // strerror
 
-#define buf_size 1024
+#define buffer_size 1024
 
 int main(int argc, char *argv[])
 {
     FILE *fp;                                            // pointer to a FILE object
-	
-    char buf[buf_size];                             
+
+    char buffer[buffer_size];
 
     for (size_t i = 1; i < argc ; i++)
     {
-        if ((fp = fopen(argv[i], "r")) == NULL)       	 // if the file cannot be opened 
+        if ((fp = fopen(argv[i], "r")) == NULL)       	 // if the file cannot be opened
         {
-            printf("wcat: cannot open file\n");
+            printf("wcat: file cannot open \n");
             exit(EXIT_FAILURE);                       	 // EXIT
         }
 
-        while (fgets(buf, buf_size, fp))         		// used to get input from file, one line at a time.
-            printf("%s", buf);                      
+        while (fgets(buffer, buffer_size, fp))         		// used to get input from file, one line at a time.
+            printf("%s", buf);
 
         errno = 0;
-        if (fclose(fp) != 0) {                       	// if the file cannot be closed 
-            strerror(errno);                       	    // error message for not closed file 
+        if (fclose(fp) != 0) {                       	// if the file cannot be closed
+            strerror(errno);                       	    // error message for not closed file
             exit(EXIT_FAILURE);
         }
     }
 
-                                
+
 
     return 0;
 }
