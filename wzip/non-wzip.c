9c48e979bf7277a6b10f224190ed273e0352321a
Ahmed Gamal Okka <56696104+ahmedokka29@users.noreply.github.com>
2022-01-07 22:40:45 +0200

    Update non-wzip.c

--- wzip/non-wzip.c
+++ wzip/non-wzip.c
@@ -19,15 +19,7 @@ int main(int argc, char* argv[]){
                 if(curr==c){
                     count+=1;
                 }else{
-                    // if(count>sizeof(int)){
-                    //     fwrite(&count, sizeof(int), 1, stdout);
-                    //     fwrite(&curr, sizeof(char), 1, stdout);
-                    // }
-                    // else{
-                    //     for(int j=1; j<=count; j++){
-                    //         fwrite(&curr, sizeof(char), 1, stdout);
-                    //     }
-                    // }
+                    
                     fwrite(&count, sizeof(int), 1, stdout);
                     fwrite(&curr, sizeof(char), 1, stdout);
                     curr = c;
